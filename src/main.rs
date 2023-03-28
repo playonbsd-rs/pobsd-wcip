45e0b5ffcd29cc5d826831b271d2c510fde28712
Hukadan <me@hukadan.org>
2023-03-28 21:17:34 +0200

    Make some improvements on error handling

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,43 @@ use pobsd_db::GameDataBase;
 use pobsd_parser::{Parser, ParserResult, Store, StoreLink};
 use std::process::Command;
 
-fn main() {
+static DB_URL: &'static str = "https://raw.githubusercontent.com/Hukadan/OpenBSD-Games-Database/add-store-links/openbsd-games.db";
+static STEAM_CTL: &'static str = "/usr/local/bin/steamctl";
+
+fn get_db() -> Result<String, attohttpc::Error> {
+    let content: String = attohttpc::get(DB_URL).send()?.text()?;
+    Ok(content)
+}
+
+fn get_steam_ids() -> Vec<usize> {
+    let output = Command::new(STEAM_CTL)
+        .arg("apps")
+        .arg("list")
+        .output()
+        .expect("Failed to execute steamctl command");
+
+    let lines =
+        std::str::from_utf8(&output.stdout).expect("Failed to convert steamctl output to string");
+    let mut ids: Vec<usize> = Vec::new();
+    for line in lines.lines() {
+        let element: Vec<&str> = line.split(" ").collect();
+        // I guess it is ok if it fails for some ids
+        if let Ok(id) = element[0].to_string().parse() {
+            ids.push(id)
+        }
+    }
+    ids
+}
+
+fn main() -> Result<(), attohttpc::Error> {
+    let db = get_db()?;
     let parser = Parser::default();
-    let games = match parser
-        .load_from_file("openbsd-games.db")
-        .expect("No db found")
-    {
+    let games = match parser.load_from_string(&db) {
         ParserResult::WithError(games, _) => games,
         ParserResult::WithoutError(games) => games,
     };
     let db = GameDataBase::new(games);
-    println!("Loading Steam data, please wait.");
-    let output = Command::new("steamctl")
-        .arg("apps")
-        .arg("list")
-        .output()
-        .unwrap();
-
-    let lines = std::str::from_utf8(&output.stdout).expect("No way");
-    let mut ids: Vec<usize> = Vec::new();
-    for line in lines.lines() {
-        let element: Vec<&str> = line.split(" ").collect();
-        ids.push(element[0].to_string().parse().expect("What?"));
-    }
+    let ids = get_steam_ids();
     for id in ids {
         if let Some(game) = db.get_game_by_steam_id(id) {
             let store = match &game.stores {
@@ -43,4 +57,5 @@ fn main() {
             println!("{}: {}", &game.name, store.url);
         }
     }
+    Ok(())
 }
