270dfcab6c90ecabb8df306ccce085397be21288
Hukadan <me@hukadan.org>
2023-04-03 08:52:59 +0200

    A bit of refactoring

---
+++ src/database.rs
@@ -0,0 +1,24 @@
+use pobsd_db::GameDataBase;
+use pobsd_parser::{Parser, ParserResult};
+
+static DB_URL: &str =
+    "https://raw.githubusercontent.com/playonbsd/OpenBSD-Games-Database/main/openbsd-games.db";
+
+fn get_db() -> Result<String, attohttpc::Error> {
+    let content: String = attohttpc::get(DB_URL).send()?.text()?;
+    Ok(content)
+}
+
+pub fn get_game_db() -> Result<GameDataBase, attohttpc::Error> {
+    let db = get_db()?;
+
+    let parser = Parser::default();
+    let games = match parser.load_from_string(&db) {
+        ParserResult::WithError(games, _) => {
+            eprintln!("A parsing error occured while parsing the PlayOnBSD database. Data might be imcomplete.");
+            games
+        }
+        ParserResult::WithoutError(games) => games,
+    };
+    Ok(GameDataBase::new(games))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,45 @@
+mod database;
 mod ls;
 mod os_support;
 mod steam_games;
-#[cfg(feature = "tui")]
-mod tui;
+
 #[macro_use]
 extern crate lazy_static;
 
+use crate::database::get_game_db;
 use crate::ls::display_game_list;
 use crate::steam_games::get_steam_games;
-#[cfg(feature = "tui")]
-use crate::tui::browse;
 use pledge::pledge_promises;
-use pobsd_db::GameDataBase;
-use pobsd_parser::{Parser, ParserResult};
 use std::boxed::Box;
 use std::error;
 
-static DB_URL: &str =
-    "https://raw.githubusercontent.com/playonbsd/OpenBSD-Games-Database/main/openbsd-games.db";
-
-fn get_db() -> Result<String, attohttpc::Error> {
-    let content: String = attohttpc::get(DB_URL).send()?.text()?;
-    Ok(content)
-}
+// TUI feature
+#[cfg(feature = "tui")]
+use crate::tui::browse;
+#[cfg(feature = "tui")]
+mod tui;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     pledge_promises![Tty Stdio Rpath Inet Dns Exec Proc]
         .or_else(pledge::Error::ignore_platform)
         .unwrap();
 
-    let db = get_db()?;
-    let parser = Parser::default();
-    let games = match parser.load_from_string(&db) {
-        ParserResult::WithError(games, _) => {
-            eprintln!("A parsing error occured while parsing the PlayOnBSD database. Data might be imcomplete.");
-            games
-        }
-        ParserResult::WithoutError(games) => games,
-    };
-    let db = GameDataBase::new(games);
+    let db = get_game_db()?;
+    // Get list of games owned and running on OpenBSD
+    // When called, drop Exec and Proc pledge promises
     let game_list = get_steam_games(db)?;
 
-    // Make a list of owned games running on OpenBSD
     pledge_promises![Tty Stdio]
         .or_else(pledge::Error::ignore_platform)
         .unwrap();
 
+    // TUI feature
     #[cfg(feature = "tui")]
     browse(game_list)?;
+
+    // Without TUI feature
     #[cfg(not(feature = "tui"))]
     display_game_list(game_list);
+
     Ok(())
 }
